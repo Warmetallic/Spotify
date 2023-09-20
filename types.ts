5c925ad52569adf191abe4d8d1512f046126b4ed
Gleb Kharlamov <scream10@list.ru>
2023-09-20 16:53:27 +0300

    deploy

--- types.ts
+++ types.ts
@@ -1,24 +1,22 @@
-import Stripe from "stripe";
+import Stripe from 'stripe';
 
-export interface UserDetails {
+export interface Song {
   id: string;
-  first_name: string;
-  last_name: string;
-  full_name?: string;
-  avatar_url?: string;
-  billing_address?: Stripe.Address;
-  payment_method?: Stripe.PaymentMethod[Stripe.PaymentMethod.Type]; 
-};
+  user_id: string;
+  author: string;
+  title: string;
+  song_path: string;
+  image_path: string;
+}
 
 export interface Product {
   id: string;
   active?: boolean;
   name?: string;
   description?: string;
-  image?:string;
-  metadata?:Stripe.Metadata
+  image?: string;
+  metadata?: Stripe.Metadata;
 }
-
 
 export interface Price {
   id: string;
@@ -35,12 +33,31 @@ export interface Price {
   products?: Product;
 }
 
+export interface Customer {
+  id: string;
+  stripe_customer_id?: string;
+}
+
+export interface UserDetails {
+  id: string;
+  first_name: string;
+  last_name: string;
+  full_name?: string;
+  avatar_url?: string;
+  billing_address?: Stripe.Address;
+  payment_method?: Stripe.PaymentMethod[Stripe.PaymentMethod.Type];
+}
+
+export interface ProductWithPrice extends Product {
+  prices?: Price[];
+}
+
 export interface Subscription {
   id: string;
   user_id: string;
   status?: Stripe.Subscription.Status;
   metadata?: Stripe.Metadata;
-  price_id?: String;
+  price_id?: string;
   quantity?: number;
   cancel_at_period_end?: boolean;
   created: string;
@@ -51,5 +68,5 @@ export interface Subscription {
   canceled_at?: string;
   trial_start?: string;
   trial_end?: string;
-  prices?: PromiseConstructor;
+  prices?: Price;
 }
